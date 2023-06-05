7a5c89b19c36c6790d747994f8c94f49cad853e8
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-05 22:08:15 +0530

    Add files via upload

---
+++ Double.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first Value:");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            value = value * 2;
+            Console.WriteLine("Result:" + value);
+        }
+    }
+}
