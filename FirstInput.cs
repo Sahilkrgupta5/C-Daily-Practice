4f48ddd617546fc1d9c7b834d2c1f167dae7ea92
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-05 18:04:50 +0530

    Add files via upload

---
+++ FirstInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FirstInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the input:");
+            string a = Console.ReadLine();
+            Console.WriteLine("Input=" + a);
+        }
+    }
+}
