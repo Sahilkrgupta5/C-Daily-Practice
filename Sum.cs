59f99b429bcbfce4dfb30fc16243533ff9f68e3d
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-05 18:12:58 +0530

    Add files via upload

---
+++ Sum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Value:");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second Value:");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            int sum = f + s;
+            Console.WriteLine("Sum of Value:" + sum);
+        }
+    }
+}
