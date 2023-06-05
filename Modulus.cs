5600df1f89fbca353054aef3cebc84641106db42
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-05 21:51:40 +0530

    Add files via upload

---
+++ Modulus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Modulus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first Value:");
+            double f = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second Value:");
+            double s = Convert.ToDouble(Console.ReadLine());
+
+            f = f % s;
+            Console.WriteLine("Remainder:" + f);
+        }
+    }
+}
