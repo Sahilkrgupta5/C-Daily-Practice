7d470a3a41ac364c87a3c947acf56ca27bee9fca
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-05 18:24:45 +0530

    Add files via upload

---
+++ Sub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Sub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Value:");
+            int f = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Second Value:");
+            int s = Convert.ToInt32(Console.ReadLine());
+
+            int sub = f - s;
+            Console.WriteLine("Subtraction of Value:" + sub);
+        }
+    }
+}
