8512ef3a815424a3b824c01d633d4af463b73458
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-05 22:42:51 +0530

    Add files via upload

---
+++ Cube.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Cube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Value:");
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            value = value * value * value;
+            Console.WriteLine("Cube:" + value);
+        }
+    }
+}
