e99b9afa454de12c79a4617e3349e21b55faf461
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-05 22:26:43 +0530

    Add files via upload

---
+++ Square.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Value:");
+            double value = Convert.ToDouble(Console.ReadLine());
+
+            value = value * value;
+            Console.WriteLine("Square:" + value);
+        }
+    }
+}
