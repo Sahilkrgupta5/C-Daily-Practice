09fc6b9fad5a786ac7751021ae646de733242b41
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-06 19:22:10 +0530

    Add files via upload

---
+++ LeapYear.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            year = year % 4;
+            if (year == 0)
+            {
+                Console.WriteLine("It is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("It is not a Leap Year");
+            }
+        }
+    }
+}
