01f758088028426580c33756a451805c76a85e39
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-06 13:44:41 +0530

    Add files via upload

---
+++ PrimeNoDoWhile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace PrimeNoDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value to check Prime = ");
+            int value = Convert.ToInt32(Console.ReadLine());
+            int i=1,prime=0;
+
+            do
+            {
+                if (value%i == 0)
+                {
+                    prime++;
+                }
+                i++;
+            } while (i<=value);
+            if(prime==2)
+            {
+                Console.WriteLine("No. is Prime = " + value);
+            }
+            else if(prime==1)
+            {
+                Console.WriteLine("No. is Prime = " + value);
+            }
+            else
+            {
+                Console.WriteLine("No. is Non Prime = " + value);
+            }
+        }
+    }
+}
