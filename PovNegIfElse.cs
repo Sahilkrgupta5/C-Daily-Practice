9428ebf23cbb503c19d5a3df9bbcd7a90ca16d71
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-06 11:32:56 +0530

    Add files via upload

---
+++ PovNegIfElse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PovNegIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Value to check Positive or Negative = ");
+            int value=Convert.ToInt32(Console.ReadLine());
+
+            if(value > 0)
+            {
+                Console.WriteLine("Value is Positive:" + value);
+            }
+            else if(value < 0)
+            {
+                Console.WriteLine("Value is Negative:" + value);
+            }
+            else
+            {
+                Console.Write("Value is Zero" + value);
+            }
+        }
+    }
+}
