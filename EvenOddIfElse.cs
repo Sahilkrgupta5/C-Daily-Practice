113f5b6311355e6413d1130d26417f53c6c628e0
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-06 11:06:08 +0530

    Add files via upload

---
+++ EvenOddIfElse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EvenOddIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the No. to find Even Or Odd : ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            if(value%2==0)
+            {
+                Console.Write("Number is Even = " + value);
+            }
+            else
+            {
+                Console.Write("Number is Odd = " + value);
+            }
+        }
+    }
+}
