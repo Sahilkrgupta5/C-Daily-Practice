2f9b31f659e0112d6bfd3b8d1b64c57c88872160
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-06 19:24:41 +0530

    Add files via upload

---
+++ CalculatorIfElse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace CalculatorIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Value:");
+            double f = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operation:");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter Second Value:");
+            double s = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                f = f + s;
+                Console.WriteLine("Result:" + f);
+            }
+            else if (op == "-")
+            {
+                f = f - s;
+                Console.WriteLine("Result:" + f);
+            }
+            else if (op == "*")
+            {
+                f = f * s;
+                Console.WriteLine("Result:" + f);
+            }
+            else if(op=="/")
+            {
+                f = f / s;
+                Console.WriteLine("Result:" + f);
+            }
+            else if(op=="%")
+            {
+                f = f % s;
+                Console.WriteLine("Result:" + f);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Kays");
+            }
+        }
+    }
+}
