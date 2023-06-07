0b92237a80b178242af1b33b74ee8b985f554e29
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-07 12:05:24 +0530

    Add files via upload

---
+++ CalculatorSwitchCase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace CalculatorSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter First Value:");
+            double f=Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operation:");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter Second Value:");
+            double s = Convert.ToDouble(Console.ReadLine());
+
+            switch(op)
+            {
+                case "+":
+                    f = f + s;
+                    Console.WriteLine("Result:" + f);
+                    break;
+                case "-":
+                    f = f - s;
+                    Console.WriteLine("Result:" + f);
+                    break;
+                case"*":
+                    f = f * s;
+                    Console.WriteLine("Result:" + f);
+                    break;
+                case"/":
+                    f = f / s;
+                    Console.WriteLine("Result:" + f);
+                    break;
+                case "%":
+                    f = f % s;
+                    Console.WriteLine("Result:" + f);
+                    break;
+                default:
+                    Console.WriteLine("Enter Valid Kays");
+                    break;
+            }
+        }
+    }
+}
