128154039d91ac137e9ff91463be4b2c2dfe5aae
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-15 00:11:36 +0530

    Add files via upload

---
+++ VoterID.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace VoterID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string na;
+            int age;
+            Console.Write("Enter Your Nationality:");
+            na = Console.ReadLine();
+            if(na.ToLower()=="indian")
+            {
+                Console.Write("Enter the Age:");
+                age = Convert.ToInt32(Console.ReadLine());
+                if(age>=18)
+                {
+                    Console.Write("You are eligible for Vote");
+                }
+                else
+                {
+                    Console.Write("You are under Age");
+                }
+            }
+            else
+            {
+                Console.Write("You are not Indian");
+            }
+
+        }
+    }
+}
