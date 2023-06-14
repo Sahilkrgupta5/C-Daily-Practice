6e8998bf9f2e982ec38700896a8c417b0575e455
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-15 00:37:01 +0530

    Add files via upload

---
+++ AlphabetSmallCapital.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AlphabetSmallCapital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Alphabet:");
+            char a = Convert.ToChar(Console.ReadLine());
+            if(a>=65 && a<=90)
+            {
+                Console.WriteLine("Alhabet is Capital");
+            }
+            else
+            {
+                Console.WriteLine("Alphabet is Small");
+            }
+        }
+    }
+}
