6b8d9617a36d6d268a2ff1e2276af2958fccf0b2
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-14 23:28:25 +0530

    Add files via upload

---
+++ Table.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Value:");
+            int a=Convert.ToInt32(Console.ReadLine());
+            int i = 1,table=0;
+
+            while(i<=10)
+            {
+                table=a* i;
+                Console.WriteLine(a + "*" + i + "=" + table);
+                i++;
+            }
+        }
+    }
+}
