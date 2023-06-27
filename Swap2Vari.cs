57993e1e707d44615316086003c2faccfc546dee
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-28 02:57:54 +0530

    Add files via upload

---
+++ Swap2Vari.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Swap2Vari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 45, b = 65;
+            Console.WriteLine("Values before Swap______________");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+
+            a = a + b;//a=110
+            b = a - b;//b=45
+            a = a - b;//a=65
+            Console.WriteLine("Values after Swap_______________");
+            Console.WriteLine("a=" + a );
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
