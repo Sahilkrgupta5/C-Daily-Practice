c4f86b16f04681a18b5510b22ff926c44b37a784
Sahilkrgupta5 <Sahilkrgupta5@gmail.com>
2023-06-28 02:39:47 +0530

    Add files via upload

---
+++ Swap2VariTemp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Swap2VariTemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20, temp;
+            Console.WriteLine("Values before Swap_____________");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+            
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("Values after Swap______________");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
